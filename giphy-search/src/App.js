c245353b3a5baee0bc47684e7d930df3a531a667
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-13 10:36:06 -0500

    Skeleton-ish

---
+++ giphy-search/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/GifCard.js'
+import GifCard from './components/GifCard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <GifCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ giphy-search/src/components/GifCard.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function GifCard() {
+
+    const [post, setPost] = useState([])
+    const [gif, setGif] = useState([])
+
+    const fetchPost = async () => {
+        const response = await axios(`http://api.giphy.com/v1/gifs/search?q=${gif}&api_key=efd9tZUH8eWQP62dcQtIZvx4K63dbt1I`)
+        setGif(response.data)
+        console.log(response.data)
+    }
+
+    useEffect( () => {
+        fetchPost()
+    }, [])
+
+    return (
+        <div className = "GifSearch">
+            <h1>Hello</h1>
+            <div>
+                <input type = "text" value = {gif} onChange = {e => setGif(e.target.value)}/>
+                <button onClick = {fetchPost}>Search</button>
+            </div>
+            <div>
+                {post.map(element => {
+                    return(
+                        <div className = "gif" key={element.id}>
+                        <div>{element.url}</div>
+                        </div>
+                    
+                )})}</div>
+        </div>
+    );
+}
+
+export default GifCard
